4ef4fc7b5d5acc6d760e58619cc41ea20ce5c6e3
Nggullu <67764690+Nggullu@users.noreply.github.com>
2020-07-21 01:12:35 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -2,6 +2,6 @@ import os
 
 
 class my():
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    BOT_TOKEN = os.environ[1264729198:AAEPoUPfuYqnBTzIOJiekjSIitIW71j55qs"]
+    API_ID = int(os.environ["1662232"])
+    API_HASH = os.environ["8f6ca7babe3abf60736c38cc6a43d273"]
